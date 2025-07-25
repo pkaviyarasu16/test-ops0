cb306c3bb83fad0f0b7f5f2517221d23c34bebfa
pkaviyarasu16 <kaviyarasu@ops0.com>
2025-07-25 23:24:44 +0530

    IaC changes

--- add643f6-1d30-4422-b574-9a2fc905f43a/main.tf
+++ add643f6-1d30-4422-b574-9a2fc905f43a/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_version = "~> 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket         = "terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
