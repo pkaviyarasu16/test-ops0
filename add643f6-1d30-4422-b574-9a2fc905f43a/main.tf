d265744bbdda8c067816b6c0872f4073ce30324c
pkaviyarasu16 <kaviyarasu@ops0.com>
2025-07-26 00:45:56 +0530

    IaC changes

--- add643f6-1d30-4422-b574-9a2fc905f43a/main.tf
+++ add643f6-1d30-4422-b574-9a2fc905f43a/main.tf
@@ -1,7 +1,65 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 0.14.0"
+}
+
 provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "dhaidhadha-jansdjmehi-kavi"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+  version  = var.kubernetes_version
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
+
+  depends_on = [
+    aws_cloudwatch_log_group.eks_cluster_logs,
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "eks_cluster_logs" {
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = var.log_retention_days
+}
+
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = var.node_group_name
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = data.aws_subnets.default.ids
+  instance_types  = [var.instance_type]
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+
+  depends_on = [
+    aws_eks_cluster.eks_cluster
+  ]
 }
--- add643f6-1d30-4422-b574-9a2fc905f43a/variables.tf
+++ add643f6-1d30-4422-b574-9a2fc905f43a/variables.tf
@@ -3,3 +3,63 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "test-eks-cluster"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version to use for the EKS cluster"
+  type        = string
+  default     = "1.27"
+}
+
+variable "cluster_role_arn" {
+  description = "ARN of the IAM role that provides permissions for the Kubernetes control plane to make calls to AWS API operations"
+  type        = string
+  default     = "arn:aws:iam::123456789012:role/eks-cluster-role"
+}
+
+variable "node_role_arn" {
+  description = "ARN of the IAM role that provides permissions for the EKS worker nodes"
+  type        = string
+  default     = "arn:aws:iam::123456789012:role/eks-node-role"
+}
+
+variable "node_group_name" {
+  description = "Name of the EKS node group"
+  type        = string
+  default     = "test-eks-nodes"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for the EKS worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "node_desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "node_max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
+
+variable "node_min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "log_retention_days" {
+  description = "Number of days to retain log events"
+  type        = number
+  default     = 7
+}
