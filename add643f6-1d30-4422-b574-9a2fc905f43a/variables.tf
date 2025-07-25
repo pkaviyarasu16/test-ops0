64e95261c386fcf87990484a403ac53a31132ee2
pkaviyarasu16 <kaviyarasu@ops0.com>
2025-07-25 22:03:53 +0530

    IaC changes

--- add643f6-1d30-4422-b574-9a2fc905f43a/variables.tf
+++ add643f6-1d30-4422-b574-9a2fc905f43a/variables.tf
@@ -1,5 +1,47 @@
-variable "region" {
-  description = "The AWS region to deploy resources"
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
   type        = string
-  default     = "us-east-1"
+  default     = "test-eks-cluster"
+}
+
+variable "cluster_role_arn" {
+  description = "ARN of the IAM role for EKS cluster"
+  type        = string
+  default     = "arn:aws:iam::123456789012:role/eks-cluster-role"
+}
+
+variable "node_role_arn" {
+  description = "ARN of the IAM role for EKS node group"
+  type        = string
+  default     = "arn:aws:iam::123456789012:role/eks-node-role"
+}
+
+variable "node_group_name" {
+  description = "Name of the EKS node group"
+  type        = string
+  default     = "test-node-group"
+}
+
+variable "desired_nodes" {
+  description = "Desired number of nodes in the node group"
+  type        = number
+  default     = 2
+}
+
+variable "min_nodes" {
+  description = "Minimum number of nodes in the node group"
+  type        = number
+  default     = 1
+}
+
+variable "max_nodes" {
+  description = "Maximum number of nodes in the node group"
+  type        = number
+  default     = 3
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+  default     = "t3.medium"
 }
