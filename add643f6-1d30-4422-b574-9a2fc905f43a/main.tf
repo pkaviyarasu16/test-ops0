c0ed3b00a0360f18493cd964142e1ecb36bbecd9
pkaviyarasu16 <kaviyarasu@ops0.com>
2025-07-27 19:59:44 +0530

    IaC changes

--- add643f6-1d30-4422-b574-9a2fc905f43a/main.tf
+++ add643f6-1d30-4422-b574-9a2fc905f43a/main.tf
@@ -27,9 +27,74 @@ data "aws_subnets" "default" {
   }
 }
 
+# IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach required policies to cluster role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# IAM Role for EKS Node Group
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach required policies to node role
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# CloudWatch Log Group for EKS Cluster
+resource "aws_cloudwatch_log_group" "eks_cluster_logs" {
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = var.log_retention_days
+}
+
+# EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
-  role_arn = var.cluster_role_arn
+  role_arn = aws_iam_role.eks_cluster_role.arn
   version  = var.kubernetes_version
 
   vpc_config {
@@ -38,18 +103,15 @@ resource "aws_eks_cluster" "eks_cluster" {
 
   depends_on = [
     aws_cloudwatch_log_group.eks_cluster_logs,
+    aws_iam_role_policy_attachment.eks_cluster_policy
   ]
 }
 
-resource "aws_cloudwatch_log_group" "eks_cluster_logs" {
-  name              = "/aws/eks/${var.cluster_name}/cluster"
-  retention_in_days = var.log_retention_days
-}
-
+# EKS Node Group
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = var.node_group_name
-  node_role_arn   = var.node_role_arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = data.aws_subnets.default.ids
   instance_types  = [var.instance_type]
 
@@ -60,6 +122,26 @@ resource "aws_eks_node_group" "eks_nodes" {
   }
 
   depends_on = [
-    aws_eks_cluster.eks_cluster
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_container_registry_policy
   ]
+}
+
+# Output for EKS Cluster Name
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.eks_cluster.name
+}
+
+# Output for EKS Cluster Endpoint
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster API server"
+  value       = aws_eks_cluster.eks_cluster.endpoint
+}
+
+# Output for kubectl configuration command
+output "kubectl_config_command" {
+  description = "Command to configure kubectl to use this EKS cluster"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.eks_cluster.name}"
 }
