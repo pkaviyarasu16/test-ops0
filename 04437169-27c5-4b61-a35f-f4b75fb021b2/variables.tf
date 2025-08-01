c24dc380b30e2e9d7086166732da452908d3df49
pkaviyarasu16 <kaviyarasu@ops0.com>
2025-08-01 23:03:17 +0530

    IaC changes

--- 04437169-27c5-4b61-a35f-f4b75fb021b2/variables.tf
+++ 04437169-27c5-4b61-a35f-f4b75fb021b2/variables.tf
@@ -1,5 +1,5 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources"
+variable "region" {
+  description = "AWS region where the S3 bucket will be created"
   type        = string
   default     = "us-east-1"
 }
