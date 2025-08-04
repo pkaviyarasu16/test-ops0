054d5607a1803553fde36132757e0f1e9a5fd29b
pkaviyarasu16 <kaviyarasu@ops0.com>
2025-08-04 23:42:45 +0530

    IaC changes

--- 04437169-27c5-4b61-a35f-f4b75fb021b2/variables.tf
+++ 04437169-27c5-4b61-a35f-f4b75fb021b2/variables.tf
@@ -9,3 +9,9 @@ variable "bucket_name" {
   type        = string
   default     = "jinininndkwbef-qerferf"
 }
+
+variable "bucket_name_2" {
+  description = "The name of the second S3 bucket"
+  type        = string
+  default     = "jinininndkwbef-qerferf-2"
+}
