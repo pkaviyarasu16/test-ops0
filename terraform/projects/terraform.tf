83e38bd5a39d9852e545939f2c2df92cf974f4ac
pkaviyarasu16 <kaviyarasu@ops0.com>
2026-01-06 19:14:00 +0530

    Update Terraform configuration

---
+++ terraform/projects/terraform.tf
@@ -0,0 +1,12 @@
+# Global Terraform Configuration
+
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
