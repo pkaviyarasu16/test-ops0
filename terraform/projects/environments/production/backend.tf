f44613e258c55c488e8fd0156c89373debe3e648
pkaviyarasu16 <kaviyarasu@ops0.com>
2026-01-06 19:14:01 +0530

    Update Terraform configuration

--- terraform/projects/environments/production/backend.tf
+++ terraform/projects/environments/production/backend.tf
@@ -1,1 +1,8 @@
-# Backend configuration
+terraform {
+  backend "s3" {
+    bucket = "34234dd2ddfsf3f34"
+    key    = "discovery/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
