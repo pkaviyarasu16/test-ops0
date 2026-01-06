594a15e84fb311fbf668e086b4701190a2942739
pkaviyarasu16 <kaviyarasu@ops0.com>
2026-01-06 19:13:50 +0530

    Update Terraform configuration

---
+++ terraform/projects/modules/vpc/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.this.id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.this.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "igw_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.this.id
+}
