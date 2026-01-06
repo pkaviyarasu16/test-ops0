59c1f966771b2b5f68a0ce4bf732770c83a5f74b
pkaviyarasu16 <kaviyarasu@ops0.com>
2026-01-06 19:13:48 +0530

    Update Terraform configuration

---
+++ terraform/projects/modules/security/variables.tf
@@ -0,0 +1,26 @@
+variable "security_groups" {
+  description = "Map of security group configurations"
+  type = map(object({
+    description = string
+    vpc_id      = string
+    ingress_rules = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
+    egress_rules = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
+  }))
+  default = {}
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
